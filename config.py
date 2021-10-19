e5b9e3d0a9fbe9fc07840a0471a22b3d23eb1009
Bhanuka Mahanama <mahanama94@gmail.com>
2021-10-19 18:16:34 -0400

    Updates to Yahoo scraping Added Reddit scraping for WSB

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 
-DATA_DIRECTORY = os.environ.get("DATA_DIRECTORY", "../data/")
+LOGGING_DIRECTORY = "../logs/"
+
+YAHOO_DATA_DIRECTORY = os.environ.get("DATA_DIRECTORY", "../data/yahoo/")
 
 REDDIT_CLIENT_ID = "ec2mtK_u5AOmuJEKvu4raQ"
 REDDIT_CLIENT_SECRET = "8TVgeCuv_gCuwSLnjJ9Ru2iMVgzCow"
 REDDIT_USER_AGENT = "NLP"
+REDDIT_DATA_DIRECTORY = "../data/reddit/"
--- data_extractors/reddit.py
+++ data_extractors/reddit.py
@@ -1,8 +1,65 @@
+import datetime
+import logging
+import csv
+
 import praw
+from config import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT, REDDIT_DATA_DIRECTORY, LOGGING_DIRECTORY
 
-from config import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT
+#
+# Best/ Confidence ?= upvotes - downvotes
+#
+# Top ?= upvotes
+#
+# Hot ?= upvotes * time
+#
 
 
-reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET, user_agent=REDDIT_USER_AGENT)
+start_date = datetime.date(2021, 10, 17)
+end_date = datetime.date(2021, 10, 19)
 
-print("Hello")
+if __name__ == '__main__':
+
+    SUBREDDIT = "wallstreetbets"
+    logging.basicConfig(filename=LOGGING_DIRECTORY + SUBREDDIT + ".log", level=logging.INFO)
+
+    reddit_client = praw.Reddit(client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET,
+                                user_agent=REDDIT_USER_AGENT)
+
+    current_date = start_date
+    time_delta = datetime.timedelta(days=1)
+
+    while current_date < end_date:
+        search_query = "Daily Discussion Thread for " + current_date.strftime("%B %d, %Y")
+
+        submissions = reddit_client.subreddit(SUBREDDIT).search(search_query)
+        try:
+            submission = next(submissions)
+            csv_file = open(REDDIT_DATA_DIRECTORY + submission.title + ".csv", "w", newline="\n")
+
+            csv_writer = csv.DictWriter(csv_file, fieldnames=["date", "subreddit", "id", "body", "parent_id", "score"])
+            csv_writer.writeheader()
+
+            comments = submission.comments.list()
+
+            for comment in comments:
+                if isinstance(comment, praw.models.reddit.more.MoreComments):
+                    comments.append(comment.comments())
+                else:
+                    csv_writer.writerow({
+                        "date": current_date.strftime("%Y-%m-%d"),
+                        "subreddit": SUBREDDIT,
+                        "id": comment.id,
+                        "body": comment.body,
+                        "parent_id": comment.parent_id,
+                        "score": comment.score
+                    })
+
+            csv_file.close()
+
+            logging.info("Completed for " + search_query + " with " + submission.title)
+
+        except StopIteration:
+            logging.error("SEARCH : No results for " + search_query)
+
+        current_date = current_date + time_delta
+
