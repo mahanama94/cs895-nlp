8f3e3750b765f7f43663c3b0a997e80fb4079df9
Bhanuka Mahanama <mahanama94@gmail.com>
2021-10-25 11:35:51 -0400

    Updates to twitter scraper

--- data_extractors/twitter.py
+++ data_extractors/twitter.py
@@ -7,8 +7,8 @@ from tweepy import Client
 from data_extractors.config import TWITTER_BEARER_TOKEN, LOGGING_DIRECTORY, TWITTER_TAG, TWITTER_DATA_DIRECTORY, \
     TWITTER_TWEET_COUNT
 
-start_date = datetime.datetime(2021, 10, 19)
-end_date = datetime.datetime(2021, 10, 24)
+start_date = datetime.datetime(2019, 10, 19)
+end_date = datetime.datetime(2019, 10, 24)
 
 if __name__ == '__main__':
 
@@ -20,9 +20,11 @@ if __name__ == '__main__':
     time_delta = datetime.timedelta(days=1)
 
     while current_date < end_date:
-        tweets = client.search_recent_tweets(query=TWITTER_TAG, start_time=current_date,
-                                             max_results=TWITTER_TWEET_COUNT,
-                                             expansions=["author_id"], tweet_fields=["created_at", "public_metrics"])
+        tweets = client.search_all_tweets(query=TWITTER_TAG, start_time=current_date,
+                                          end_time=current_date+time_delta,
+                                          max_results=TWITTER_TWEET_COUNT,
+                                          expansions=["author_id"], tweet_fields=["created_at", "public_metrics"],
+                                          place_fields=["country_code"])
 
         csv_file = open(TWITTER_DATA_DIRECTORY + TWITTER_TAG + "-" + current_date.strftime("%Y-%m-%d") + ".csv",
                         "w", newline="\n")
