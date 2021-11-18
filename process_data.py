4207d64f529f24f4e3d61c1291825a18fc17f6ba
pilemendoza <71232719+pilemendoza@users.noreply.github.com>
2021-11-18 16:59:56 -0500

    Code for merging

---
+++ process_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+volume_df = pd.read_csv("data/options/GME-volume.csv")
+
+volume_df = volume_df.groupby(by="Trade Date").sum()
+volume_df["Date"] = volume_df.index.str.replace("/", "-")
+
+volatility_df = pd.read_csv("data/options/GME-volatility.csv")
+volatility_df["Date"] = volatility_df["Date"].str.replace("/", "-")
+
+volatility_df["Date"].dtype = str
+
+merged = volume_df.merge(volatility_df, left_on='Date', right_on='Date')
+merged.to_csv("data/options/GME-merged.csv")
+
+volume_df.to_csv("data/options/GME-volume-processed.csv")
+
+volume_df.head()
