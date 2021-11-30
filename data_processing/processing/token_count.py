1d2080080a3b04c216e5ed8837f95962088cdc64
Bhanuka Mahanama <mahanama94@gmail.com>
2021-11-30 15:25:20 -0500

    token counts

--- data_processing/processing/token_count.py
+++ data_processing/processing/token_count.py
@@ -8,24 +8,29 @@ def run(parser):
     subparser = argparse.ArgumentParser(parents=[parser])
 
     subparser.add_argument("--combined-file", type=str, default="processed_data/reddit-combined.csv")
-    subparser.add_argument("--token", type=str, default="GME")
-    subparser.add_argument("--output-data-dir", type=str, default="processed_data/")
+    subparser.add_argument("--tokens", type=str, default="GME,gme,#GME,$GME,G M E,GMEE,GMEEE,GMEEEE,GMEEEEE,"
+                                                         "GMEEEEEE,Game Stop,Gamestop,GameStop,GAMESTOP,Mr. Cohen,"
+                                                         "Cohen,DADDY COHEN")
+    subparser.add_argument("--output-file", type=str, default="processed_data/token-counts.csv")
 
     args = subparser.parse_args()
 
     combined_file_name = args.combined_file
-    token = args.token
-    output_data_path = args.output_data_dir
-
-    logging.info("Completed token occurrences for {}".format(token))
+    tokens = args.tokens.split(",")
+    output_file = args.output_file
 
     df = pd.read_csv(combined_file_name)
 
-    df["token_count"] = df["body"].astype("str").map(lambda body: body.count(token))
+    for token in tokens:
+        logging.info("Completed token occurrences for {}".format(token))
+
+        df[token] = df["body"].astype("str").map(lambda body: body.count(token))
+
+        logging.info("Completed token occurrences for {}".format(token))
 
     token_occurrences = df.groupby("date").sum()
 
-    token_occurrences.to_csv(output_data_path + "token-count-" + token + ".csv", columns=["token_count"])
+    token_occurrences.to_csv(output_file, columns=tokens)
 
-    logging.info("Completed token occurrences for {}".format(token))
+
 
