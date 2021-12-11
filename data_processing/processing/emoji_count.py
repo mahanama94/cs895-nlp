62959d0d10a872f33b9aae7777d0669513462b91
Bhanuka Mahanama <mahanama94@gmail.com>
2021-12-12 05:25:37 +0530

    Added code for emoji counts

---
+++ data_processing/processing/emoji_count.py
@@ -0,0 +1,26 @@
+import argparse
+
+import emoji
+import pandas as pd
+
+
+def run(parser):
+    subparser = argparse.ArgumentParser(parents=[parser])
+
+    subparser.add_argument("--combined-file", type=str, default="processed_data/reddit-combined.csv")
+    subparser.add_argument("--output-file", type=str, default="processed_data/emoji-counts.csv")
+
+    args = subparser.parse_args()
+
+    combined_file_name = args.combined_file
+    output_file = args.output_file
+
+    emojicons = ["😎"]
+
+    df = pd.read_csv(combined_file_name)
+    for emojicon in emojicons:
+        df[emoji.demojize(emojicon).replace(":", "")] = df["body"].astype("str").map(lambda body: body.count(emojicon))
+
+    emoji_occurences = df.groupby("date").sum()
+
+    emoji_occurences.to_csv(output_file, columns=list(map(lambda x: emoji.demojize(x).replace(":", ""), emojicons)))
--- process_data.py
+++ process_data.py
@@ -3,19 +3,21 @@ import logging
 
 from data_processing.processing.combine_files import run as run_combine_files
 from data_processing.processing.token_count import run as run_token_count
+from data_processing.processing.emoji_count import run as run_emoji_count
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
     function_map = {
         "combine-files": run_combine_files,
-        "token-count": run_token_count
+        "token-count": run_token_count,
+        "emoji-count": run_emoji_count
     }
 
     parser = argparse.ArgumentParser(description="Data processing for Reddit-Options dataset", add_help=False)
 
     parser.add_argument("--action", choices=function_map.keys(), default="token-count")
 
-    args, unknown_ards = parser.parse_known_args()
+    args, unknown_args = parser.parse_known_args()
 
     function_map.get(args.action)(parser)
